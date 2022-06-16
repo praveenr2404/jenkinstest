d266e19acff0eea48071f47d6cc7b048e6fe8d9e
prave <praveenr2404@gmail.com>
2022-06-15 21:24:22 -0400

    added subtest project

--- jenkins-sub-test/src/main/java/com/Application.java
+++ jenkins-sub-test/src/main/java/com/Application.java
@@ -5,9 +5,8 @@ import java.io.File;
 public class Application {
     public static void main(String[] args) {
         String filePath = new File(System.getProperty("user.dir")+"/out/test.txt").getAbsolutePath();
+        System.out.println(filePath);
         WriteFile.write(filePath);
         System.out.println("Write Success");
-        System.out.println(System.getProperty("user"));
-        System.out.println(System.getProperty("pwd"));
     }
 }
