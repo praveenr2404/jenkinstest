3ac39e31a979d7dd55ec7973df4c27b477d33059
prave <praveenr2404@gmail.com>
2022-06-15 21:32:01 -0400

    added subtest project

--- jenkins-sub-test/src/main/java/com/WriteFile.java
+++ jenkins-sub-test/src/main/java/com/WriteFile.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 public class WriteFile {
     public static void write(String filePath){
         try(FileWriter fWriter = new FileWriter(filePath,true);
-            PrintWriter pWriter = new PrintWriter(fWriter);
+            PrintWriter pWriter = new PrintWriter(fWriter)
         ){
             pWriter.println("abcd");
         }catch (IOException e){
